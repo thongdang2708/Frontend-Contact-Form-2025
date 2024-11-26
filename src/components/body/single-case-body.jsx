b20706c7e36984118536ede4f7801cb1957cf185
Thong Dang <duythong2708@gmail.com>
2024-11-26 23:09:38 +0200

    Update footer imports

--- src/components/body/single-case-body.jsx
+++ src/components/body/single-case-body.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../header/Header'
 import SingleCase from './single-case'
-import Footer from '../footer/footer'
+import Footer from '../footer/Footer'
 
 function SingleCaseBody() {
   return (
